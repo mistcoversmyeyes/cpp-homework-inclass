74aab85e397d7cb6a1db1e11ba9ce2c71aa5c898
linux owned by yuming <mingjiangyu1@qq.com>
2023-10-12 14:58:07 +0000

    可以运行的版本“
    git add homework1.cpp

--- homework1.cpp
+++ homework1.cpp
@@ -1,20 +1,27 @@
 #include<iostream>
 using namespace std;
 
-int Maximun(int length,int number[]) {
-	int SizeOfNumbers=length ;
-	for (int i=0;i<SizeOfNumbers;i++) {
+int  findMax(int length ,int number[]) {
+	for (int i=0;i<length;i++) {
 		int max = 0;
-			if(number[i] < max) {
+			if(number[i] >max) {
 				max=number[i];
 			}
 		
-		return max;
 	}
 }
 
 int main() {
-	int length ;
-	cout << "你要输入几个数:" << endl;
-	cin >> length;
-	int numbers[length];}
+	int lengthOfNumbers;
+	int max=0;
+	cout<< "你要输入几个数:" << endl;
+	cin >> lengthOfNumbers;
+	int *numbers=new  [lengthOfNumbers];
+	for(int i=0;i<lengthOfNumbers;i++){
+		cin>>numbers[i];
+	}
+	
+	max=findMax(lengthOfNumbers,numbers);
+	cout<<"这些数中的最大值是"<< max <<endl;//调用FindMax函数的返回值作为输出
+	}
+	

ca94ed0936b2c385010ca16aba0e120828848a07
linux owned by yuming <mingjiangyu1@qq.com>
2023-10-13 02:05:36 +0000

    最新更改

--- homework1.cpp
+++ homework1.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int  findMax(int length ,int number[]) {
+	int max = 0;
 	for (int i=0;i<length;i++) {
-		int max = 0;
 			if(number[i] >max) {
 				max=number[i];
 			}
 		
-	}
+	};
+	return max ;
 }
 
 int main() {
@@ -16,7 +17,7 @@ int main() {
 	int max=0;
 	cout<< "你要输入几个数:" << endl;
 	cin >> lengthOfNumbers;
-	int *numbers=new  [lengthOfNumbers];
+	int *numbers=new int [lengthOfNumbers];
 	for(int i=0;i<lengthOfNumbers;i++){
 		cin>>numbers[i];
 	}

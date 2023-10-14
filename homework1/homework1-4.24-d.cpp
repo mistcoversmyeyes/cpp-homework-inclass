35a8c231d7cb6084c3a6e06d6e79dc8c4d8d02b7
linux owned by yuming <mingjiangyu1@qq.com>
2023-10-14 08:22:39 +0000

    “作业4.24已完成 git commit -m作业4.24已完成
    cd
    v

---
+++ homework1/homework1-4.24-d.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=0;
+    int y=0;
+    cout<<"请输入y的值"<<endl;
+    cin>>y;
+    cout<<"请输入x的值"<<endl;
+    cin>>x;
+    if(y==8){
+        if(x==5)
+        cout<<"@@@@@"<<endl;}
+        else
+        cout<<"#####"<<endl;
+    cout<<"$$$$$"<<endl;
+    cout<<"&&&&&"<<endl;
+    return 0;
+
+}

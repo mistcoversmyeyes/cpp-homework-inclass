b8844ce98064dd462b3a01e59aaf9f273ed7506b
linux owned by yuming <mingjiangyu1@qq.com>
2023-10-19 08:10:36 +0000

    优化了使用泰勒级数计算e的n次方的程序，将main 函数前置了

--- homework1/homework1-4.34/homework-4.34-c.cpp
+++ homework1/homework1-4.34/homework-4.34-c.cpp
@@ -1,5 +1,35 @@
 #include<iostream>
 using  namespace std;
+double power_of(int base,int exponent );
+double factorial_of(int n);
+double calculate_e(int terms,int x);
+
+
+int main(){
+    //从用户获取需要计算的exponent,和迭代的次数
+    int x = 0;
+    int step = 0;
+    double result = 0;
+    char continueChoice;
+
+    do {
+        cout << "请输入你要计算e的几次方: " << endl;
+        cin >> x;
+        cout << "请输入你要迭代的次数：" << endl;
+        cin >> step;
+
+        result = calculate_e(step, x);
+        cout << "e的n次方的近似值是: " << result << endl;
+
+        cout << "您想要继续计算吗? (y/n): ";
+        cin >> continueChoice;
+    } while (continueChoice == 'y' || continueChoice == 'Y');
+
+    return 0;
+}
+
+
+//计算给定底数的乘方
 double power_of(int base,int exponent ){
     int result=1;
     for(int i=0 ; i < exponent; i++){
@@ -30,29 +60,4 @@ double calculate_e(int terms,int x){
     }
     return result;
 }
- 
-int main(){
-
-    int x = 0;
-    int step = 0;
-    double result = 0;
-    char continueChoice;
-
-    do {
-        cout << "请输入你要计算e的几次方: " << endl;
-        cin >> x;
-        cout << "请输入你要迭代的次数：" << endl;
-        cin >> step;
-
-        result = calculate_e(step, x);
-        cout << "e的n次方的近似值是: " << result << endl;
-
-        cout << "您想要继续计算吗? (y/n): ";
-        cin >> continueChoice;
-    } while (continueChoice == 'y' || continueChoice == 'Y');
-
-    return 0;
-}
-
-
 
